05ebc4f3fa27582ea62813b67a742568e44551ae
Capel001 <willychang008@gmail.com>
2019-09-09 14:30:41 +0800

    update

--- public/js/sevenSeg.js
+++ public/js/sevenSeg.js
@@ -1,59 +1,81 @@
 //menu
+$(function () {
+    $.ajax({
+        dataType: "json",
+        url: "../menu.json",
+        success: function(data){
+            var menuHtml = "";
+            jQuery.each(data,function(key,value){
+                menuHtml += "<li><a href=\"/"+ value + "\">" + key + "</a></li>\n";
+            });
+
+            $("nav ul").append(menuHtml);
+        }
+    });
+
+
+});
+
+//sign in
+$(function(){
+    let $alertMessage = $(".alertMessage");
+    $alertMessage.hide();
+    $('.signIn').on('click',function(event){
+         $alertMessage.slideDown(400);
+        event.preventDefault();
+        $('.cancel').on("click",function(event){
+            $alertMessage.slideUp(400);
+        })
+    });
+
+});
+
+//menu
 $(function(){
     var menuState = false;
-    var $list = $(".list");
-    if(window.innerWidth<767){
-        $(".list").hide();
-    }
-    $("#menu").on('click',function(event){
+    $("#menu").on("click touchstart",function(event){
         menuState = !menuState;
-        if(menuState){
+        let smallWindow = $(window).width() <= 767;
+        if (menuState && smallWindow) {
             console.log("menu open");
-            $(this).attr("src","../images/cross.png");
-            $list.slideDown(300);
-        }
-        else{
+            $(this).attr("src","images/close.png");
+            $("nav>ul").css("display","block");
+        }else{
             console.log("menu close");
-            $(this).attr("src","../images/menu.png");
-            $list.slideUp(300);
+            $(this).attr("src","images/menu.png");
+            $("nav>ul").css("display","none");
         }
-
-        $(window).resize(function(event){
-        //console.log(window.innerWidth);
-        if(window.innerWidth>767){
-            $(".list").show();
-        }
-        else{
-            $(".list").hide();
+        event.preventDefault();
+    });
+    $(window).resize(function(event){
+        //console.log(window.innerWidth)
+        if(window.innerWidth > 767){
+            $("nav>ul").show();
         }
     });
-    });
-
-    
 });
 
 // Initialize Firebase
-var database = firebase.database();
+let database = firebase.database();
 var btnState;
 
 //firebase
-$(document).ready(function(){
-    database.ref("iot0624/LED").on('value',function(snapshot){
-        //console.log(snapshot.val())
-        btnState = snapshot.val();
-        if(btnState){
-            $("a.btn").addClass("btn-open");
-            $(".status").html("OPEN");
-        }
-        else{
-            $("a.btn").removeClass("btn-open");
-            $(".status").html("CLOSE");
-        }
 
+$(document).ready(function(){
+
+     //read firebase data of digit node
+      let iot0624Ref = database.ref('iot0624');
+      iot0624Ref.child("digit").on('value',function(snapshot){
+        let selectedValue = snapshot.val();
+        $(".digits button").removeClass("selected").each(function(index,element){
+            if(index == selectedValue){
+               $(element).addClass("selected");
+            }
+        });
     });
 
-    $("a.btn").on("click",function(event){
-        event.preventDefault();
-        database.ref("iot0624/LED").set(!btnState);
-    })
+    $(".digits button").on("click",function (event) {
+        let contentText = event.target.innerText;
+        iot0624Ref.update({'digit':parseInt(contentText)});
+    });
 });
